635a1232e3af50a0e24f3dc57e517d31d93aed6d
Luke <colored.vertices@gmail.com>
2025-04-27 15:38:37 -0300

    refactor: IPs substituídos

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$servername = "SERVIDOR";
+$username = "USUÁRIO";
+$password = "SENHA";
+$database = "BANCO";
 
 // Criar conexão
 
